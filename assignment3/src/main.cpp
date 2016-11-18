169e086f826b16bab1b105fad9ef1f51eec4c5ee
olihoops <nguyenolive@gmail.com>
2016-11-17 19:40:54 -0500

    added test cases

--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -10,20 +10,55 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-  Graph g;
+  {
+    Graph g;
+    g.retrieve("short_path.in");
+    Vertex v1 = g.vertex("DC");
+    Vertex v2 = g.vertex("LIB");
+    static const int arr_closure[] = {0, 2, 4, 5};
+    vector<int> ground_truth_closure(arr_closure, arr_closure + sizeof(arr_closure) / sizeof(arr_closure[0]) );
+    vector<int> output = g.trip(v1,v2);
+    assert(output == ground_truth_closure);
+  }
 
- //	g.printGraph();
-  g.retrieve("dijkstra.in");
+  {
+    Graph g;
+    g.retrieve("longer_path.in");
+    Vertex v1 = g.vertex("DC");
+    Vertex v2 = g.vertex("LIB");
+    static const int arr_closure[] = {0, 2, 4, 5};
+    vector<int> ground_truth_closure(arr_closure, arr_closure + sizeof(arr_closure) / sizeof(arr_closure[0]) );
+    vector<int> output = g.trip(v1,v2);
+    assert(output == ground_truth_closure);
+  }
 
-  Vertex v1 = g.vertex("DC");
-  Vertex v2 = g.vertex("LIB");
+  {
+    Graph g;
+    g.retrieve("normal.in");
 
-  static const int arr[] = {0, 2, 4, 1, 5};
-  vector<int> ground_truth(arr, arr + sizeof(arr) / sizeof(arr[0]) );
-  vector<int> output = g.trip(v1,v2);
-  assert(output == ground_truth);
+    Vertex v1 = g.vertex("DC");
+    Vertex v2 = g.vertex("LIB");
 
-  g.store("dijkstra.out");
+    static const int arr[] = {0, 2, 4, 1, 5};
+    vector<int> ground_truth(arr, arr + sizeof(arr) / sizeof(arr[0]) );
+    vector<int> output = g.trip(v1,v2);
+    assert(output == ground_truth);
+  }
+
+  // Test w/ road closure
+
+  {
+    Graph g;
+    g.retrieve("closure.in");
+    Vertex v1 = g.vertex("DC");
+    Vertex v2 = g.vertex("LIB");
+    static const int arr_closure[] = {0, 2, 4, 5};
+    vector<int> ground_truth_closure(arr_closure, arr_closure + sizeof(arr_closure) / sizeof(arr_closure[0]) );
+    vector<int> output = g.trip(v1,v2);
+    assert(output == ground_truth_closure);
+  }
+
+  //g.store("dijkstra.out");
 
   return 0;
 }
